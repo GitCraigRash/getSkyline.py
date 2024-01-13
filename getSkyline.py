f162fbe8aa1eeb7f760eda74784ea70fb37f0385
Craig Rash Jr <90971454+GitCraigRash@users.noreply.github.com>
2024-01-12 23:02:50 -0500

    Update getSkyline.py

--- getSkyline.py
+++ getSkyline.py
@@ -1,3 +1,18 @@
 class Solution:
     def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
-        
+        # Approch one:
+        #Left to Right
+        #Case Omega: No buildings
+        # Case One: One building
+        # Case two : two seperate buildings
+        # Case three: two prefectly overlapping buidlings
+        # Case four : two overlapping with one higher than another
+        # Case five : two overlapping with one wider than the other
+        # Case six : three seperate buildings
+        # Case seven : three prefectly overlapping buildings
+        # Case eight : two overlaping and one seperate building
+        # Case nine : three overlaping buildings the second is lower than first, third overlaps second and is taller. 
+        # Case ten :
+        # Approch two: 
+        # Shortest to tallest.
+        # Problem: What if a huge building completely covers all other smaller buildings? Won't you have to reduce the number of nodes?
